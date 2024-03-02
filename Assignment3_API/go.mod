6f71df9988850b636b5caba8083fa2d520e7268e
vsys-shamkamble <sham.kamble@vedanshis.com>
2024-03-02 17:49:31 +0530

    files added

---
+++ Assignment3_API/go.mod
@@ -0,0 +1,5 @@
+module workspace
+
+go 1.22.0
+
+require github.com/gorilla/mux v1.8.1
---
+++ prolocal/second.go
@@ -0,0 +1,18 @@
+package main 
+
+import (
+
+    "fmt"
+    "github.com/shamdkamble/Go-Learn"
+    "time"
+
+)
+
+func main () {
+
+a := test.DoSomething()
+fmt.Print(a)
+b:= time.Now()
+
+fmt.Print(b)
+}
